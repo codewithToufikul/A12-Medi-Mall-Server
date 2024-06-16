a90b52f8c14fd0dc755b198b04ba2bb05f2188bb
Toufikul Islam <142156125+imran8990@users.noreply.github.com>
2024-06-16 12:47:27 +0600

    category data , cart data and cart all operation added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ivo4yuq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
@@ -28,6 +28,7 @@ async function run() {
     await client.connect();
     const medicineCollection = client.db("MediMallDB").collection("medicineCollection");
     const cartMedicineCollection = client.db("MediMallDB").collection("cartMedicineCollection");
+    const catergoryCollection = client.db("MediMallDB").collection("catergoryCollection");
     
     app.get("/medicine", async(req, res) => {
         const result = await medicineCollection.find().toArray();
@@ -37,6 +38,55 @@ async function run() {
     app.post("/cart", async(req, res)=>{
       const medicine = req.body;
       const result = await cartMedicineCollection.insertOne(medicine);
+      res.send(result)
+    })
+
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const emailQuery = {
+        userEmail: email,
+      };
+      const result = await cartMedicineCollection.find(emailQuery).toArray();
+      res.send(result);
+    });
+
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartMedicineCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
+    app.patch("/cart/:id/increase", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $inc: { quantity: 1 },
+      };
+      const result = await cartMedicineCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+    
+    app.patch("/cart/:id/decrease", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $inc: { quantity: -1 },
+      };
+      const result = await cartMedicineCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+    
+    app.delete("/cart", async (req, res) => {
+      const email = req.query.email;
+      const emailQuery = { userEmail: email };
+      const result = await cartMedicineCollection.deleteMany(emailQuery);
+      res.send(result);
+    });
+
+    app.get("/category", async(req, res)=>{
+      const result = await catergoryCollection.find().toArray();
       res.send(result)
     })
 
