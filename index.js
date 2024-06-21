db53db1d1cf0646103ab303d3998e6947c3d5767
Toufikul Islam <142156125+imran8990@users.noreply.github.com>
2024-06-21 09:35:55 +0600

    seller dashboard addvice data get

--- index.js
+++ index.js
@@ -43,17 +43,20 @@ async function run() {
 
   const verifyToken = (req, res, next) => {
     if (!req.headers.authorization) {
-      return res.status(401).send({ message: 'forbidden access' });
+        return res.status(401).send({ message: 'Unauthorized access' });
     }
     const token = req.headers.authorization.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) {
-        return res.status(401).send({ message: 'forbidden access' });
-      }
-      req.decoded = decoded;
-      next();
+        if (err) {
+            return res.status(401).send({ message: 'Unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
     });
-  };
+};
+
+
+  
 
   const verifyAdmin = async (req, res, next) => {
     const email = req.decoded.email;
@@ -71,34 +74,34 @@ async function run() {
     res.send(result);
   });
 
-  app.get('/users/admin/:email', verifyToken, async (req, res) => {
+  app.get("/users/admin/:email", verifyToken, async (req, res) => {
     const email = req.params.email;
-  
+
     if (email !== req.decoded.email) {
-      return res.status(403).send({ message: 'Forbidden access' });
+        return res.status(403).send({ message: 'Forbidden access' });
     }
-  
+
     try {
-      const query = { email: email };
-      const user = await usersCollections.findOne(query);
-      let admin = false;
-      if (user) {
-        admin = user.role === 'admin';
-      }
-      res.send({ admin });
+        const query = { email: email };
+        const user = await usersCollections.findOne(query);
+        let admin = false;
+        if (user) {
+            admin = user.role === 'admin';
+        }
+        res.send({ admin });
     } catch (error) {
-      console.error('Error fetching admin status:', error);
-      res.status(500).send({ message: 'Failed to fetch admin status' });
+        console.error('Error fetching admin status:', error);
+        res.status(500).send({ message: 'Failed to fetch admin status' });
     }
-  });
+});
+
 
   app.get('/users/seller/:email', verifyToken, async (req, res) => {
     const email = req.params.email;
-  
+   
     if (email !== req.decoded.email) {
       return res.status(403).send({ message: 'Forbidden access' });
     }
-  
     try {
       const query = { email: email };
       const user = await usersCollections.findOne(query);
@@ -144,7 +147,7 @@ async function run() {
     res.send(result);
   });
 
-  app.get("/carts", verifyToken, async (req, res) => {
+  app.get("/carts", async (req, res) => {
     const email = req.query.email;
     const emailQuery = { userEmail: email };
     const result = await cartMedicineCollection.find(emailQuery).toArray();
